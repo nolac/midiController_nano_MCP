4637ac59763afc00508d22f2774df07df8ed5b24
nolac <nolac2@gmail.com>
2018-11-13 10:35:43 +0100

    adding a page led control and a debouce on page change button

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,15 @@ MidiPad pad;
 uint16_t oldValue(0);
 uint16_t currentValue(0);
 bool oldPageChanged=0;
+bool page=0;
 //bool oldF2=0,oldF1=0,oldF3=0;
 
 void setup()
 {
   pad.init();
   mcp.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN,page);
   pinMode(PAGE, INPUT_PULLUP);
   //pinMode(F2,INPUT_PULLUP);
   for (int i = 0; i < 16; i++)
@@ -43,8 +46,12 @@ void loop()
   // page 1 from 60 to 75
   // page 2 from 76 to 82
   bool pageChanged = !digitalRead(PAGE);
+  delay(5);
   if((pageChanged==true)&&(pageChanged!=oldPageChanged)){
     pad.switchPage();
+    page=!page;
+    digitalWrite(LED_BUILTIN, page);
+
   }oldPageChanged=pageChanged;
 
   sendControle();
